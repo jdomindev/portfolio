bd0aa1a1d1ffdfff40aea57d590f152103c82be7
Jose Dominguez <jdomindev@gmail.com>
2023-03-02 21:09:05 -0500

    added footer component and styles, added more app.css styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
 import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <div className="App">
-      <header>
-
-      </header>
+      <Header />
       <main id="content-wrap">
-      
-      </main>
-      <footer>
 
-      </footer>
-      
+      </main>
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import '../styles/footer.css'
+
+function Footer() {
+
+  return (
+    <footer>
+        <div className='footer'>
+            <a className='footer-email' href="mailto:jdomindev@gmail.com">jdomindev@gmail.com</a>
+            <h6>© 2023 Copyright: Jose Dominguez</h6>
+        </div>
+        <div>
+
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
