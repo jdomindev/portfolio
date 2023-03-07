89ee493871838ca261e769b34cadb0e01486ce87
Jose Dominguez <jdomindev@gmail.com>
2023-03-06 21:08:04 -0500

    added header, header.css, bootstrap installed, react-router-dom installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
-    <div id="page-container">
-      <Header />
-      <main id="content-wrap">
+    <>
+      <div id="page-container">
+        <Header />
+        <main id="content-wrap">
 
-      </main>
-      <Footer />
-    </div>
+        </main>
+        <Footer />
+      </div>
+
+      <Routes>
+        {/* <Route path="/about" element={<Header />}/> */}
+        {/* <Route path="/about" element={<About />} /> */}
+      </Routes>
+    </>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,35 @@
 import '../styles/header.css'
+import { NavLink, Link } from 'react-router-dom'
+
 
 function Header() {
-
   return (
     <header>
-      <h1>JD</h1>
-      <button></button>
+      <nav className='navbar navbar-dark navbar-expand-lg'>
+        <div className='container-fluid'>
+          <Link className='navbar-brand' to="/">
+            <h1>JD</h1>
+          </Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="navbar-collapse collapse" id="navbarNav">
+            <div className="navbar-nav ms-auto">
+              <NavLink to="/about" className="navlink">
+                About Me
+              </NavLink>
+              <NavLink to="/projects" className="navlink">
+                Projects
+              </NavLink>
+              <Link to="https://drive.google.com/file/d/1URkiU2vqHl6UBqxUWpSQpxsIAiFqyd6W/view?usp=share_link" 
+                target="_blank" rel="noreferrer"
+                className="navlink">
+                Resume
+              </Link>
+            </div>
+          </div>
+        </div>
+      </nav>
     </header>
   )
 }
