74c1df91f2e206020312438f3c2d009a953b52af
Jose Dominguez <jdomindev@gmail.com>
2023-03-27 17:18:18 -0400

    added app.js routing, and app css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import About from './pages/About'
+import Projects from './pages/Projects'
+import Landing from './pages/Landing'
 import { Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -10,15 +13,14 @@ function App() {
       <div id="page-container">
         <Header />
         <main id="content-wrap">
-
+        <Routes>
+          <Route path="/" element={<Landing />}/>
+          <Route path="/about" element={<About />}/>
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
         </main>
         <Footer />
       </div>
-
-      <Routes>
-        {/* <Route path="/about" element={<Header />}/> */}
-        {/* <Route path="/about" element={<About />} /> */}
-      </Routes>
     </>
   )
 }
