d96acba5903297ff79d9539013fbfc84c52892bc
Jose Dominguez <jdomindev@gmail.com>
2023-03-27 17:16:23 -0400

    Added landing page and css

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,19 @@
+import "../styles/landing.css";
+import "../App.css";
+
+function Landing() {
+  return (
+    <section className="container">
+        <div className="image-container">
+            <img className="img-landing" src="/profile.jpg"></img>
+            <div>
+                <h1 className="landing-header">Jose Dominguez</h1>
+                <h2 className="landing-subheader">Web Developer</h2>
+            </div>
+            <h3 className="brand-statement">My aim is to build enjoyable user experiences that help people get the information they need with ease</h3>
+        </div>
+    </section>
+  );
+}
+
+export default Landing;
