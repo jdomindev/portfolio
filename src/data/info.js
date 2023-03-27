cb6ca1de635027c31569c2e133bcb8d75d66e362
Jose Dominguez <jdomindev@gmail.com>
2023-03-27 17:45:42 -0400

    allow project data to be uploaded

---
+++ src/data/info.js
@@ -0,0 +1,131 @@
+export const info = {
+  tech: {
+    frontend: [
+      {
+        icon: "fa-brands fa-react",
+        name: "React",
+      },
+      {
+        icon: "fa-brands fa-html5",
+        name: "HTML",
+      },
+      {
+        icon: "fa-brands fa-css3-alt",
+        name: "CSS",
+      },
+      {
+        icon: "fa-brands fa-square-js",
+        name: "JavaScript",
+      },
+      {
+        icon: "fa-brands fa-square-js",
+        name: "Mapbox",
+      },
+      {
+        icon: "fa-brands fa-square-js",
+        name: "Bootstrap",
+      },
+      {
+        icon: "fa-brands fa-square-js",
+        name: "Highcharts",
+      },
+      {
+        icon: "fa-brands fa-square-js",
+        name: "D3",
+      },
+      {
+        icon: "fa-brands fa-git-alt",
+        name: "Git",
+      },
+      {
+        icon: "fa-brands fa-npm",
+        name: "npm",
+      },
+    ],
+    backend: [
+      {
+        icon: "fa-brands fa-node-js",
+        name: "Node.js",
+      },
+      {
+        icon: "fa-brands fa-node-js",
+        name: "Express",
+      },
+      {
+        icon: "fa-brands fa-node-js",
+        name: "MongoDB",
+      },
+      {
+        icon: "fa-brands fa-node-js",
+        name: "GraphQL",
+      },
+      {
+        icon: "fa-brands fa-node-js",
+        name: "MySQL",
+      },
+    ],
+  },
+  projects: [
+    {
+      title: "The WRK Group's Social Impact",
+      type: "Employer: Tech Impact",
+      role: "Designer | Developer",
+      date: "2023",
+      image: "wrk-group.png",
+      description:
+        "Data dashboard built for The WRK Group to quantify and illustrate their social impact across various domains such as housing, education, and safety.",
+      tech: [
+        "HTML",
+        "CSS",
+        "JS",
+        "React",
+        "React Router",
+        "D3",
+        "High Charts",
+        "Bootstrap",
+        "Cloudflare",
+      ],
+      repoLink: "https://github.com/de-data-lab/WRK-react",
+      deployedLink: "https://wrk-group-social-impact.pages.dev/",
+    },
+    {
+      title: "Tandem Fandom",
+      type: "Academic Project",
+      role: "Designer | Developer",
+      date: "2022",
+      image: "tandem.png",
+      description:
+        "MERN stack app designed for those seeking to find and match with users with similar fandom interests. Created back-end by combining a Node.js/Express.js server and MongoDB/GraphQL database and query language. Utilized a React front-end supplemented with bootstrap, react-bootstrap, and react router.",
+      tech: [
+        "HTML",
+        "CSS",
+        "JS",
+        "React",
+        "React Router",
+        ,
+        "Node.js",
+        "Express",
+        "MongoDB",
+        "GraphQL",
+        "Bootstrap",
+        "JWT"
+      ],
+      repoLink: "https://github.com/jdomindev/tandemfandom",
+      deployedLink: "https://thawing-island-96191.herokuapp.com/",
+    },
+    {
+      title: "Game Getter",
+      type: "Academic Project",
+      role: "Designer | Developer",
+      date: "2022",
+      image: "game-getter.png",
+      description:
+        "App where you can search for the best deals for any video game you desire. Leverages RawG and Cheapshark APIs to find the best deals for video games from across the web.",
+      tech: ["HTML", "CSS", "JS", "jQuery", "Materialize", "REST APIs"],
+      repoLink: "https://github.com/jdomindev/game-getter",
+      deployedLink: "https://jdomindev.github.io/game-getter/",
+    },
+  ],
+};
+
+export default info;
