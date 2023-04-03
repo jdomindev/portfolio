333f3efb1e8c923ee25215a762a7eb2ad75d6bb0
Jose Dominguez <jdomindev@gmail.com>
2023-04-03 18:01:29 -0400

    customized bootstrap navbar toggler color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ function Header() {
           <Link className='header-text' to="/">
             <h1>Jose Dominguez</h1>
           </Link>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <button className="custom-toggler navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="navbar-collapse collapse" id="navbarNav">
